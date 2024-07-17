44fc937336beb7c4cda53e37dc1e4dd1caac976c
Jannick Pedersen <jannickpedersen@live.dk>
2024-07-17 13:13:21 +0200

    implemented sms with Twilio through appwrite

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -124,9 +124,11 @@ export const updateAppointment = async ({appointmentId, userId, appointment, typ
 
         if (!updatedAppointment) throw Error;
 
-        const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for 
+        const smsMessage = `Greetings from CarePulse. 
+        ${type === "schedule" ? `Your appointment is confirmed for 
         ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for 
         ${formatDateTime(appointment.schedule!).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
+        
         await sendSMSNotification(userId, smsMessage);
 
         revalidatePath("/admin");
