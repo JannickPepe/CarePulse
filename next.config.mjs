68d488d8207cab01b19defbf28a822df42ed1522
Jannick Pedersen <jannickpedersen@live.dk>
2024-11-09 21:51:03 +0100

    had to change next config name back so its sync to sentry cli

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ export default withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 org: "jannickdev",
-project: "VibingHealth",
+project: "carepulse",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
